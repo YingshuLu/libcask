dfe9d5b2aa4109c9b2e23470b54a4ddd2eab22a8
yingshulu <hhulys@163.com>
2018-08-09 10:23:10 +0800

    fix stack pop

--- stack.h
+++ stack.h
@@ -10,6 +10,6 @@ typedef deque_t stack_t;
 #define stack_empty(s)   deque_empty((s))
 #define stack_size(s)    deque_size((s))
 #define stack_push(s, e) deque_push_back((s), (e))
-#define stack_pop(s)     deque_pop_back((s), (e))
+#define stack_pop(s)     deque_pop_back((s))
 
 #endif
