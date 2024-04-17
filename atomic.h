83b3dc37fb0ee1bff7109e117cc3d3785cf40794
kamlu <kamlu@tencent.com>
2024-04-17 23:12:36 +0800

    use gcc atomic

--- atomic.h
+++ atomic.h
@@ -11,7 +11,7 @@ typedef atomic_t atomic_int_t;
 
 #define LOCK "lock;"
 #define INLINE __inline__ __attribute__((always_inline))
-#define atomic_init(t) (atomic_set(t, 0))  
+#define atomic_init(t) (atomic_set(t, 0))
 #define atomic_get(t) ACCESS_ONCE((t)->v)
 #define atomic_set(t, n) (((t)->v) = (n))
 #define atomic_add_and_fetch(t, n) __sync_add_and_fetch(&((t)->v), n)
@@ -22,72 +22,20 @@ typedef atomic_t atomic_int_t;
 #define atomic_sub(t, n) atomic_sub_and_fetch(t, n)
 #define atomic_compare_and_swap(t, o, n) __sync_bool_compare_and_swap(&((t)->v), (o), (n))
 
-INLINE int atomic_sub_and_test(atomic_t* t, int i) {
-    unsigned char res;
-    __asm__ __volatile__ (
-        LOCK "subl %2, %0; sete %1"
-        :"=m" (t->v), "=qm" (res)
-        :"ir" (i), "m" (t->v) : "memory");
-    return res;
-}
-
-INLINE int atomic_add_and_test(atomic_t* t, int i) {
-    unsigned char res;
-    __asm__ __volatile__ (
-        LOCK "addl %2, %0; sete %1"
-        :"=m" (t->v), "=qm" (res)
-        :"ir" (i), "m" (t->v) : "memory");
-    return res;
-}
-
 INLINE void atomic_inc(atomic_t *t) {
-    __asm__ __volatile__ (
-    LOCK "incl %0"
-    :"=m" (t->v)
-    :"m" (t->v));
+    __atomic_fetch_add(&t->v, 1, __ATOMIC_SEQ_CST);
 }
 
 INLINE void atomic_dec(atomic_t *t) {
-    __asm__ __volatile__ (
-    LOCK "decl %0"
-    :"=m" (t->v)
-    :"m" (t->v));
-}
-
-INLINE int atomic_inc_and_test(atomic_t *t) {
-    unsigned char res;
-    __asm__ __volatile__ (
-    LOCK "incl %0; sete %1"
-    :"=m" (t->v), "=qm" (res)
-    :"m" (t->v) : "memory");
-    return res;
-}
-
-INLINE int atomic_dec_and_test(atomic_t *t) {
-    unsigned char res;
-    __asm__ __volatile__ (
-    LOCK "decl %0; sete %1"
-    :"=m" (t->v), "=qm" (res)
-    :"m" (t->v) : "memory");
-    return res;
+    __atomic_fetch_sub(&t->v, 1, __ATOMIC_SEQ_CST);
 }
 
 INLINE int atomic_dec_and_negative(atomic_t *t) {
-    unsigned char res;
-    __asm__ __volatile__ (
-    LOCK "decl %0; sets %1"
-    :"=m" (t->v), "=qm" (res)
-    :"m" (t->v) : "memory");
-    return res;
+    return atomic_sub_and_fetch(t, 1) < 0? 1 : 0;
 }
 
 INLINE int atomic_inc_and_negative(atomic_t *t) {
-    unsigned char res;
-    __asm__ __volatile__ (
-    LOCK "incl %0; sets %1"
-    :"=m" (t->v), "=qm" (res)
-    :"m" (t->v) : "memory");
-    return res;
+   return atomic_add_and_fetch(t, 1) < 0? 1 : 0;
 }
 
 #endif
